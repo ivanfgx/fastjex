cd0debe9a6bdeebea10b1e9b3f3e66e0bdc92d9f
vanoushka <vanoushka3011@gmail.com>
2015-04-12 09:03:46 -0500

    PAYMENTS

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
-ruby '2.2.1'
 gem 'rails', '4.2.0'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
@@ -7,6 +6,18 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
+gem 'bourbon'
+gem 'bower-rails', '~> 0.9.2'
+gem "rest-client"
+gem 'paypal-sdk-adaptivepayments'
+gem "pp-adaptive"
+gem 'whenever', :require => false
+gem 'susy'
+gem 'font-awesome-sass'
+gem 'jquery-turbolinks'
+gem "rest-client"
+gem 'paypal-sdk-adaptivepayments'
+gem "pp-adaptive"
 group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
@@ -16,7 +27,6 @@ gem 'devise'
 gem 'devise-i18n'
 gem 'figaro', '>= 1.0.0.rc1'
 gem 'mysql2'
-gem 'pundit'
 group :development do
   gem 'hub', :require=>nil
   gem 'rails_layout'
---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,112 @@
+class PaymentsController < ApplicationController
+  require 'date'
+  require 'paypal-sdk-adaptivepayments'
+  require 'rest_client'
+  require "pp-adaptive"
+
+
+  def check_for_payments
+    
+  def test
+    result = Payment.new
+    result = result.pay(User.last, Payment.last)
+    render json: result
+  end
+
+  def pay
+    payment = Payment.find(params[:id])
+    @api = PayPal::SDK::AdaptivePayments.new
+
+    # Build request object
+    @pay = @api.build_pay({
+      :actionType => "PAY",
+      :cancelUrl => "http://localhost:3000/samples/adaptive_payments/pay",
+      :currencyCode => payment.currency.abbreviation,
+      :senderEmail => payment.user.email,
+      :ipnNotificationUrl => "http://localhost:3000/samples/adaptive_payments/ipn_notify",
+      :receiverList => {
+        :receiver => [{
+          :amount => payment.amount,
+          :email => payment.creditor.email }] },
+      :returnUrl => "http://localhost:3000/samples/adaptive_payments/pay" })
+
+    # Make API call & get response
+    @response = @api.pay(@pay)
+
+    # Access response
+    if @response.success? && @response.payment_exec_status != "ERROR"
+      @response.payKey
+      redirect_to @api.payment_url(@response)  # Url to complete payment
+    else
+      @response.error[0].message
+      render json: @response
+    end
+    
+  end
+
+  def subscription
+    #PA-2BS25581P84232213
+    payment = Payment.find(params[:id]);
+    client = AdaptivePayments::Client.new(
+      :user_id       => "dude_api1.hotmail.es",
+      :password      => "WC89FAW4E6N5PEKA",
+      :signature     => "AFcWxV21C7fd0v3bYYYRCpSSRl31ACwd6g7hhhBCeUn.ndSBp5HmqJIl",
+      :app_id        => "APP-80W284485P519543T",
+      :sandbox       => true
+    )
+
+    client.execute(:Pay,
+      :preapproval_key => payment.pre_approval_key,
+      :action_type     => "PAY",
+      :receiver_email  => payment.creditor,
+      :receiver_amount => payment.amount,
+      :currency_code   => payment.currency.abbreviation,
+      :cancel_url      => "https://lvh.me:3000/cancel",
+      :return_url      => "https://lvh.me:3000/"
+    ) do |response|
+
+      if response.success?
+        puts "Pay key: #{response.pay_key}"
+        puts "Status: #{response.payment_exec_status}"
+      else
+        puts "#{response.ack_code}: #{response.error_message}"
+      end
+      render json: response
+    end
+  end
+
+  def preapprovalkey
+    payment = Payment.find(params[:id])
+    client = AdaptivePayments::Client.new(
+      :user_id       => "dude_api1.hotmail.es",
+      :password      => "WC89FAW4E6N5PEKA",
+      :signature     => "AFcWxV21C7fd0v3bYYYRCpSSRl31ACwd6g7hhhBCeUn.ndSBp5HmqJIl",
+      :app_id        => "APP-80W284485P519543T",
+      :sandbox       => true
+    )
+
+    client.execute(:Preapproval,
+      :ending_date      => payment.end_date,
+      :starting_date    => payment.start_date,
+      :max_total_amount => BigDecimal(1999.00),
+      :currency_code    => params[:currency],
+      :cancel_url       => "http://lvh.me:3000/cancelled",
+      :return_url       => "http://lvh.me:3000/subs/"+params[:id]
+    ) do |response|
+      # you may alternatively pass a PreapprovalRequest instance
+
+      if response.success?
+        puts "Preapproval key: #{response.preapproval_key}"
+
+        # send the user to PayPal to give their approval
+        # e.g. https://www.paypal.com/webscr?cmd=_ap-preapproval&preapprovalkey=abc
+        redirect_to client.preapproval_url(response)
+        
+      else
+        puts "#{response.ack_code}: #{response.error_message}"
+        render json: response
+      end
+
+    end
+  end
+end
